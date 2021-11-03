1d484cb969f9378dcbbaf0636ad289fa6493b1f4
nicole0818 <on100.kawai.shin@gmail.com>
2021-11-04 07:39:19 +0900

    ユーザー管理機能２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
    validates :nickname,presence: true     
-   validates :last_name_kanji,presence: true
-   validates :first_name_kanji,presence: true
-   validates :last_name_kana,presence: true
-   validates :first_name_kana,presence: true
-   validates :birthday,presence: true
 
+   with_options presence: true, format: { with: /\A[一-龥]+\z/, message: 'に漢字を使用してください' } do
+    validates :last_name_kanji
+    validates :first_name_kanji
+  end
+  
+   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'にひらがなを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+   
+   validates :birthday,presence: true
+   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
+   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と数字の両方を含めて設定してください'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,13 @@ require 'rails_helper'
   end
 
    describe "ユーザー新規登録" do
+    context'ユーザー登録が出来る場合'do
+    it '全ての項目が入力出来ていれば登録できる' do
+      expect(@user).to be_valid
+    end
+   end
+
+    context'ユーザー登録が出来ない場合'do
     it "nicknameが空では登録できない" do
       @user.nickname = ''
       @user.valid?
@@ -22,6 +29,69 @@ require 'rails_helper'
         expect(@user.errors.full_messages).to include("Password can't be blank")
    end
 
+   it "last_name_kanjiが空では登録できない" do
+    @user.last_name_kanji = ''
+    @user.valid?
+    expect(@user.errors.full_messages).to include("Last name kanji can't be blank")
+end
+
+it "first_name_kanjiが空では登録できない" do
+  @user.first_name_kanji = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("First name kanji can't be blank")
+end
+
+it "last_name_kanjiとfirst_name_kanjiはカタカナでは登録できない" do
+  @user.last_name_kanji = 'タナカ'
+  @user.first_name_kanji = 'イチロウ'
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Last name kanji に漢字を使用してください", "First name kanji に漢字を使用してください")
+end
+
+
+it "last_name_kanaが空では登録できない" do
+  @user.last_name_kana = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+end
+
+it "first_name_kanaが空では登録できない" do
+  @user.first_name_kana = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("First name kana can't be blank")
+end
+
+it "last_name_kanaとfirst_name_kanaは漢字では登録できない" do
+  @user.last_name_kana = '田中'
+  @user.first_name_kana = '一朗'
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Last name kana にひらがなを使用してください", "First name kana にひらがなを使用してください")
+end
+
+it "birthdayが空では登録できない" do
+  @user.birthday = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Birthday can't be blank")
+end
+
+   it "英字のみのpasswordでは登録できない" do
+        @user.password = 'testtest'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には半角英字と数字の両方を含めて設定してください")
+   end
+
+   it "数字のみのpasswordでは登録できない" do
+    @user.password = '1111111'
+    @user.valid?
+    expect(@user.errors.full_messages).to include("Password には半角英字と数字の両方を含めて設定してください")
+end
+
+it "全角文字のpasswordでは登録できない" do
+  @user.password = 'Test0818'
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Password には半角英字と数字の両方を含めて設定してください")
+end
+
       it "passwordとpassword_confirmationは同一の値でなくてはならない" do
         @user.password_confirmation = 'test0000'
         @user.valid?
@@ -38,6 +108,14 @@ require 'rails_helper'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
    end
-    end
 
+   it '重複したemailが存在する場合登録できない' do
+    @user.save
+    another_user = FactoryBot.build(:user)
+    another_user.email = @user.email
+    another_user.valid?
+    expect(another_user.errors.full_messages).to include('Email has already been taken')
+  end
+  end
  end
+end
