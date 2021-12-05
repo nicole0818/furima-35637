09ba6982c984c90bc349c8cd86513c36ad6b5d48
nicole0818 <on100.kawai.shin@gmail.com>
2021-12-06 04:16:24 +0900

    商品削除機能１

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
 
-  resources :products, only: [:new,:create,:index,:show,:edit,:update]
+  resources :products, only: [:new,:create,:index,:show,:edit,:update,:destroy]
 end
